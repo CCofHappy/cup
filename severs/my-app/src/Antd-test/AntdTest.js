49906912175e855dba1c677c53061cbde1277e0a
383674420@qq.com <383674420@qq.com>
2016-11-14 17:30:13 +0800

    'update'

--- severs/my-app/src/Antd-test/AntdTest.js
+++ severs/my-app/src/Antd-test/AntdTest.js
@@ -2,57 +2,155 @@
  * Created by Administrator on 2016/11/9.
  */
 import React from 'react';
-import {Button,Col,Row,Table} from 'antd';
-import  request from 'superagent'
-import 'antd/dist/antd.css'
+import {Button,Col,Row,Table,Modal,Form,Input,Radio,message} from 'antd';
+const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
+const RadioButton = Radio.Button;
+import  request from 'superagent';
+import 'antd/dist/antd.css';
 
 var header = [
     {title:'id',dataIndex:'id'},
     {title:'name',dataIndex:'name'},
     {title:'age',dataIndex:'age'},
     {title:'sex',dataIndex:'sex'},
-    {title:'single',dataIndex:'single'}
+    {
+        title:'single',
+        dataIndex:'single',
+        // render:function(single,obj){
+        //     return(
+        //         <div>{single ? '单身狗':'恩爱狗'}</div>
+        //     )
+        // },
+        render:(single)=>(<div>{single ? '单身狗':'恩爱狗'}</div>)
+    }
 ];
 
 var data = [
 
 ];
-const getAll='http://101.200.129.112:9527/react1/student/';
+const api='http://101.200.129.112:9527/react1/student/';
 
 var ReactTest = React.createClass({
     getInitialState:function () {
         return{
-            loading:false,
-            items:[]
+            loading:true,
+            items:[],
+            modal:false,
+            name:'',
+            age:'',
+            sex:'',
+            single:null
         }
     },
     render:function () {
         return(
-            <div>
+            <div className="table-box">
                 <br/>
                 <h3>动脑学院学生信息系统</h3>
-                <Row>
-                    <Col>
-                        <Table dataSource={this.state.items} columns={header}/>
-                    </Col>
-                </Row>
-                <br/><br/>
+                <div className="table-content">
+                    <br/>
+                    <Button icon="plus" type="primary" onClick={this.handleAdd}>增加</Button>&nbsp;
+                    <Button icon="edit" type="ghost">编辑</Button>&nbsp;
+                    <Button icon="delete">删除</Button>&nbsp;
+                    <br/><br/>
+                    <Table loading={this.state.loading} dataSource={this.state.items} columns={header}/>
+                </div>
+                <Modal
+                    visible={this.state.modal}
+                    onCancel={(e)=>this.setState({modal:false})}
+                    onOk={this.handleSave}
+                    title='增加信息'
+                >
+                    <Form>
+                        <FormItem
+                            label='姓名'
+                            labelCol={{span:3}}
+                            wrapperCol={{span:19}}
+                        >
+                            <Input value={this.state.name} onChange={(e)=>this.handleChange(e.target.value,'name')}/>
+                        </FormItem>
+                        <FormItem
+                            label='年龄'
+                            labelCol={{span:3}}
+                            wrapperCol={{span:19}}
+                        >
+                            <Input value={this.state.age} onChange={(e)=>this.handleChange(e.target.value,'age')}/>
+                        </FormItem>
+                        <Row>
+                            <Col span={12}>
+                                <FormItem
+                                    label='性别'
+                                    labelCol={{span:6}}
+                                    wrapperCol={{span:12}}
+                                >
+                                    <RadioGroup value={this.state.sex} onChange={(e)=>this.handleChange(e.target.value,'sex')}>
+                                        <RadioButton key="boy" value={'boy'}>男</RadioButton>
+                                        <RadioButton key="girl" value={'girl'}>女</RadioButton>
+                                    </RadioGroup>
+                                </FormItem>
+                            </Col>
+                            <Col span={12}>
+                                <FormItem
+                                    label='单身'
+                                    labelCol={{span:6}}
+                                    wrapperCol={{span:16}}
+                                >
+                                    <RadioGroup value={this.state.single} onChange={(e)=>this.handleChange(e.target.value,'single')}>
+                                        <RadioButton key="single" value={true}>单身狗</RadioButton>
+                                        <RadioButton key="no_single" value={false}>恩爱狗</RadioButton>
+                                    </RadioGroup>
+                                </FormItem>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Modal>
             </div>
         )
+    },
+    handleSave(){
+        var that = this;
+        var data = {
+            name:this.state.name,
+            age:this.state.age,
+            sex:this.state.sex,
+            single:this.state.single,
+        };
+        request
+            .post(api)
+            .send(data)
+            .end(function (err,res) {
+                console.log(res.body);
+                that.setState({
+                    modal:false
+                });
+                message.success('提交成功！');
+            });
+    },
+    handleChange(value,type){
+        var obj ={};
+        obj[type]=value;
+        this.setState(obj);
     },
     componentDidMount(){
         var that = this;
         request
-            .get(getAll)
+            .get(api)
             .end(function (err,res) {
                 res.body = res.body.map(function (obj) {
                         obj.key = obj.id;
                         return obj;
                 });
                 that.setState({
-                    items:res.body
+                    items:res.body,
+                    loading:false
                 })
             })
+    },
+    handleAdd(){
+        this.setState({
+            modal:true
+        })
     }
 
 });
